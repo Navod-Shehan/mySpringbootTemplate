720fba5770b990bc9596bdd28b4a6038ec0ef0b1
Navod-Shehan <84204209+Navod-Shehan@users.noreply.github.com>
2023-04-07 09:56:30 +0530

    Add files via upload
    mySpringbootTemplateFirstCommit

---
+++ myTemplate/myTemplate/myTemplate/src/main/java/com/springboot/myTemplate/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.myTemplate.repository;
+
+import com.springboot.myTemplate.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <UserEntity, Integer> {
+}
---
+++ myTemplate/myTemplate/myTemplate/src/test/java/com/springboot/myTemplate/MyTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.myTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
